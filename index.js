b3465f66a234059dfb8ce971f31d5ef8a4c87214
joy-mahmud <joymahmudiucse@gmail.com>
2023-12-30 00:44:12 +0600

    drag and drop added

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
-        const taskCollection = client.db('taskDb').collection('tasks') 
-        const userCollection = client.db('taskDb').collection('users') 
+        const taskCollection = client.db('taskDb').collection('tasks')
+        const userCollection = client.db('taskDb').collection('users')
         //users related api
-        app.post('/users', async(req, res) => {
+        app.post('/users', async (req, res) => {
             const user = req.body
             console.log(user)
             //checking user exist or not
@@ -41,26 +41,60 @@ async function run() {
         })
 
         //task related api
-        app.post('/addTask', async(req,res)=>{
+        app.post('/addTask', async (req, res) => {
             const task = req.body
             const result = await taskCollection.insertOne(task)
             res.send(result)
         })
-        app.get('/getAllTask',async(req,res)=>{
-            const email =req.query.email
-            const pending = {status:'pending',email:email}
-            const ongoing = {status:'ongoing',email:email}
-            const completed = {status:'completed',email:email}
+        app.get('/getAllTask', async (req, res) => {
+            const email = req.query.email
+            const pending = { status: 'pending', email: email }
+            const ongoing = { status: 'ongoing', email: email }
+            const completed = { status: 'completed', email: email }
             const pendingTask = await taskCollection.find(pending).toArray()
             const ongoingTask = await taskCollection.find(ongoing).toArray()
             const completedTask = await taskCollection.find(completed).toArray()
-            const result ={pendingTask,ongoingTask,completedTask}
+            const result = { pendingTask, ongoingTask, completedTask }
             res.send(result)
         })
-        app.delete('/deleteTask/:id',async(req,res)=>{
-            const id =req.params.id
+        app.get('/getATask/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await taskCollection.findOne(query)
+            res.send(result)
+        })
+        app.patch('/updateTask/:id', async (req, res) => {
+            const id = req.params.id
+            const task = req.body
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    title: task.title,
+                    deadline: task.deadline,
+                    priority: task.priority,
+                    description: task.description
+                }
+            }
+            const result = await taskCollection.updateOne(query, updateDoc)
+            res.send(result)
+        })
+        app.patch('/updateStatus/:id', async (req, res) => {
+            const id = req.params.id
+            const currentStatus = req.body
+            console.log(currentStatus)
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    status:currentStatus.status
+                }
+            }
+            const result = await taskCollection.updateOne(query, updateDoc)
+            res.send(result)
+        })
+        app.delete('/deleteTask/:id', async (req, res) => {
+            const id = req.params.id
             console.log(id)
-            const query ={_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await taskCollection.deleteOne(query)
             res.send(result)
         })
@@ -70,14 +104,14 @@ async function run() {
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-       // await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('task-manager is running')
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Task manager is running on port ${port}`)
 })
